481ef28954b4fe34539f7d9045f0aaaf4a9bd1fa
Olha Pylypova <olhasve@gmail.com>
2021-02-22 20:06:11 +0200

    corr infinity scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,45 +1,45 @@
 
-// import { listItem   } from './refs'
-
-
-// export default function infinityScroll( ) {
-//     const options = {       
-//      rootMargin: '10px',
-//      // root: listGallery,
-//        //threshold: 1.0
-//     }
-//     const obs = function (entries,  observer) {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//              //image()
-//               observer.unobserve(listItem);
-              
-
-//             }
-//         }) 
-  
-     
-//   };
-//   const observer = new IntersectionObserver(obs, options);
-//    console.log("lastElementChild", listItem)
-//   observer.observe(listItem);
- 
-  
-// }
-
+import { listGallery } from './refs'
+console.log('listGallery', listGallery)
 import fetchImage from './searchImage';
 
-function  infinityScroll  ()  {
-  window.addEventListener('scroll', scrollInfinity)
-  function scrollInfinity() {
-    const scrollHeight = document.documentElement.scrollHeight;
-    const scrollTop = document.documentElement.scrollTop;
-    const imageHeight = document.documentElement.imageHeight;
 
-    if (scrollTop + imageHeight > scrollHeight - 3) {
-      setTimeout(fetchImage(), 500);
+export default function infinityScroll() {
+    let options = {       
+      rootMargin: '300px',
+      root: null,
+      threshold: 1
     }
+    let obs = function (entries,  observer) {
+      entries.forEach(entry => {
+          console.log('obs', observer)
+            if (entry.isIntersecting) {
+            fetchImage()                    
+        }
+        observer.unobserve(entry.target);
+        console.log('targ', entry.target);
+         observer.observe(listGallery.lastElementChild);
+        })       
   };
-};
+  const observer = new IntersectionObserver(obs, options);
+   console.log("last", listGallery.lastElementChild)
+  observer.observe(listGallery.lastElementChild);
+  
+}
 
-export default infinityScroll;
+// 
+
+// function  infinityScroll  ()  {
+//   window.addEventListener('scroll', scrollInfinity)
+//   function scrollInfinity() {
+//     const scrollHeight = document.documentElement.scrollHeight;
+//     const scrollTop = document.documentElement.scrollTop;
+//     const imageHeight = document.documentElement.imageHeight;
+
+//     if (scrollTop + clientHeight > scrollHeight - 3) {
+//       setTimeout(fetchImage(), 1000);
+//     }
+//   };
+// };
+
+//export default infinityScroll;
