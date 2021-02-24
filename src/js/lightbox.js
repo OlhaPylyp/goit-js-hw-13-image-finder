2ee5d48e076c441906213ce0542602d1cd04b887
Olha Pylypova <olhasve@gmail.com>
2021-02-24 15:00:47 +0200

    correct win scroll

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -7,16 +7,17 @@ import 'basicLightbox/src/styles/main.scss';
     listGallery.addEventListener('click', openModal)
     function openModal(event) {
         
+   
         if (event.target.nodeName !== "IMG") {
-            console.log('enter')
-            return;
+                return;     
         }
         
         const { source } = event.target.dataset;
         const { alt } = event.target;
         basicLightbox.create(`
 		<img width="1200" height="900" src="${source}" alt='${alt}'>
-	`).show();
+	`).show(); 
+        
     }
 }
     
--- src/js/searchImage.js
+++ src/js/searchImage.js
@@ -14,25 +14,27 @@ searchForm.addEventListener('submit',  searchFounder)
     
 loadBtn.addEventListener('click', fetchImage);
 function searchFounder(event) {
-      
+        scrollScr = false;
     event.preventDefault();
     const form = event.currentTarget;
     api.query = form.elements.query.value;
-    if (api.query === "") {
-  notice({
-  text: "enter querly"
+   
+    if (api.query === '') {
+  error({
+      text: "enter querly",
+      delay: 600
   });
         listGallery.innerHTML = '';
         return
     }
     
     console.log('api.query',  listGallery.innerHTML)
+    listGallery.innerHTML = '';      
+    api.resetPage();    
+    fetchImage();
+     form.reset();
     listGallery.innerHTML = '';
-    fetchImage()  
-               api.resetPage();
-    form.reset(); 
-        
-            lightBox();
+    lightBox();
 }
  let scrollScr = false;
 function fetchImage() {
@@ -46,18 +48,19 @@ function fetchImage() {
              gallery(image);   
             
            
-             if (listGallery.childNodes.length > 0) {
-                 if (scrollScr) {
-                     window.scrollTo(                 
-                         {
-                             top: document.documentElement.scrollTop + document.documentElement.clientHeight,
-                             behavior: 'smooth'
-                            }
-                            );
-                        }
-                    }                  
-               scrollScr = true;
-               infinityScroll();
+        if (listGallery.childNodes.length > 0) {
+            if (scrollScr) {
+                window.scrollTo(
+                    {
+                        top: document.documentElement.scrollTop + document.documentElement.clientHeight,
+                        behavior: 'smooth'
+                    }
+                );
+            }
+                 
+        }          
+        scrollScr = true;
+        infinityScroll();       
          })
      }
     
